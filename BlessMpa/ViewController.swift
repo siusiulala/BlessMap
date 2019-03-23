010e6a8951b8252939cf9ec9482408140b5f003e
Sam Yang <>
2019-03-23 13:35:50 +0800

    First commit

---
+++ BlessMpa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlessMpa
+//
+//  Created by Sam Yang on 2019/3/23.
+//  Copyright © 2019 siusiulala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
